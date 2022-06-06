86bef383f0de7452f9d6e2659721a3ecc87d177b
coton <coton.chen@gmail.com>
2022-06-06 23:15:59 +0800

    ✨ feat(/pages/Hackathons): 新增列表点击进入详细页

--- src/nifities_assets/src/pages/Hackathons/components/list.tsx
+++ src/nifities_assets/src/pages/Hackathons/components/list.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import { Hackathon, fetchAllByStatus } from '../../../api/hackahton';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import PageError from 'components/500';
+import { Link } from "react-router-dom";
 
 
 const queryClient = new QueryClient();
@@ -32,31 +34,33 @@ function ListComponent(props: {
     const { status, error, data } = useQuery<Hackathon[], Error>(`hackathons/status/${props.status}`, () => fetchAllByStatus(props.status));
 
     if (status == "loading") {
-        return <p>Loading...</p>
+        return <div className='w-screen my-[2.06rem] px-[2.92rem] text-center text-4xl'>Loading...</div>
     }
 
     if (error) {
-        return <p>An error has occurred: {error.message}</p>
+        return <PageError />;
     }
 
     return (
         <div>
             {data?.map(({ hackathon_id, title, date, description, sponsored, status, image, discord_url }, index) => (
                 <div className="inline-flex flex-col space-y-[0.5rem] items-start justify-end px-5 pt-7 bg-white border rounded-lg border-grey-300 font-Urbanist" style={{ width: 400, height: 640, }} key={hackathon_id}>
-                    <img className="w-full h-1/2 bg-gray-300 rounded-lg" src={image} />
-                    <p className="w-auto h-8 text-2xl font-semibold leading-loose text-left text-brand-text-black">{title}</p>
-                    <p className="w-auto h-5 text-sm font-semibold leading-tight text-brand">{date}</p>
-                    <p className="w-full h-1/5 text-sm font-semibold leading-tight text-brand-text-gray">{description}</p>
-                    <p className="w-auto h-5 text-sm font-semibold leading-tight text-gray-500">Sponsored by</p>
-                    <div className="flex flex-row">
-                        {sponsored.split(',').map((url, index) => (
-                            <div key={index}>
-                                <img className="w-9 h-9 mr-2" src={url} />
-                            </div>
-                        ))}
-                    </div>
+                    <Link to={`/hackathons/${hackathon_id}`} className=" contents">
+                        <img className="w-full h-1/2 bg-gray-300 rounded-lg" src={image} />
+                        <p className="w-auto h-8 text-2xl font-semibold leading-loose text-left text-brand-text-black">{title}</p>
+                        <p className="w-auto h-5 text-sm font-semibold leading-tight text-brand my-[0.59rem]">{date}</p>
+                        <p className="w-full h-1/5 text-sm font-semibold leading-tight text-brand-text-gray">{description}</p>
+                        <p className="w-auto h-5 text-sm font-semibold leading-tight text-gray-500">Sponsored by</p>
+                        <div className="flex flex-row">
+                            {sponsored.split(',').map((url, index) => (
+                                <div key={index}>
+                                    <img className="w-9 h-9 mr-2" src={url} />
+                                </div>
+                            ))}
+                        </div>
+                    </Link>
                     <div className="inline-flex flex-row items-center justify-between py-[1.5rem]">
-                        <button className="text-sm leading-snug text-brand w-[10.88rem] h-7 py-1 bg-white border rounded border-brand mr-[0.81rem]">Join discord</button>
+                        <a className="text-sm leading-snug text-brand w-[10.88rem] h-7 py-1 bg-white border rounded border-brand mr-[0.81rem] text-center" href={discord_url} rel="external" title="Go to the Discroid channel" target="_blank">Join discord</a>
                         <button className="text-sm leading-snug bg-brand w-[10.88rem] h-7 py-1 border rounded text-white">Join now</button>
                     </div>
                 </div>
