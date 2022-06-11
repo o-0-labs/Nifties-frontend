d7f4bfda7e66a9299af2efef26e50b867df63ac3
coton <coton.chen@gmail.com>
2022-06-11 14:49:41 +0800

    ✨ feat(/Layout/Footer): 加入已经完成的页面链接

--- src/nifities_assets/src/Layout/Footer/index.tsx
+++ src/nifities_assets/src/Layout/Footer/index.tsx
@@ -9,15 +9,15 @@ const Footer = () => {
         <div className='footer_menu_wrap flex_left flex_start flex_wrap'>
             <div className='flex_column footer_menu_list'>
                 <h5 className='footer_menu_title'>Magic</h5>
-                <a className='footer_menu_item' href="">Hackathons</a>
-                <a className='footer_menu_item' href="">Grants</a>
-                <a className='footer_menu_item' href="">Agora</a>
+                <a className='footer_menu_item' href="/#/Hackathons">Hackathons</a>
+                <a className='footer_menu_item' href="/#/Grants">Grants</a>
+                <a className='footer_menu_item' href="/#/Agora">Agora</a>
                 <a className='footer_menu_item' href="">Promotion</a>
-                <a className='footer_menu_item' href="">Muse</a>
+                <a className='footer_menu_item' href="/#/Muse">Muse</a>
             </div>
             <div className='flex_column footer_menu_list'>
                 <h5 className='footer_menu_title'>Community</h5>
-                <a className='footer_menu_item' href="">Pona</a>
+                <a className='footer_menu_item' href="/#/Pona">Pona</a>
                 <a className='footer_menu_item' href="">Discord</a>
             </div>
             <div className='flex_column footer_menu_list'>
