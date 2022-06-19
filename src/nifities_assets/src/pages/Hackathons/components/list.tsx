b9c0c890d46f51f86d0055e107e52061b60a733b
coton <coton.chen@gmail.com>
2022-06-19 09:18:54 +0800

    ✨ feat(/pages/Hackathons): 修改排版样式

--- src/nifities_assets/src/pages/Hackathons/components/list.tsx
+++ src/nifities_assets/src/pages/Hackathons/components/list.tsx
@@ -8,7 +8,7 @@ import JoinNowDialog from './dialog';
 
 
 const queryClient = new QueryClient();
-
+// const cover0Image = require('static/hackathons-list-cover-1.jpg');
 export default function HackahtonsList(props: {
     children?: React.ReactNode;
     status: string;
@@ -22,8 +22,8 @@ export default function HackahtonsList(props: {
 
     return (
         <QueryClientProvider client={queryClient}>
-            <ListComponent status={props.status} onOpenJoinNowDialogChange={handleOpenJoinNowDialogChange}/>
-            <JoinNowDialog open={openJoinNowDialog} onOpenJoinNowDialogChange={handleOpenJoinNowDialogChange}/>
+            <ListComponent status={props.status} onOpenJoinNowDialogChange={handleOpenJoinNowDialogChange} />
+            <JoinNowDialog open={openJoinNowDialog} onOpenJoinNowDialogChange={handleOpenJoinNowDialogChange} />
         </QueryClientProvider>
     );
 };
@@ -52,7 +52,7 @@ function ListComponent(props: {
     }
 
     // API返回错误
-    if(data.code !== 0) {
+    if (data.code !== 0) {
         return <PageError />;
     }
 
@@ -61,24 +61,22 @@ function ListComponent(props: {
     return (
         <div>
             {Hackathons.map(({ hackathon_id, title, date, description, sponsored, status, image, discord_url }, index) => (
-                <div className="inline-flex flex-col space-y-[0.5rem] items-start justify-end px-5 pt-7 bg-white border rounded-lg border-grey-300 font-Urbanist" style={{ width: 400, height: 640, }} key={hackathon_id}>
+                <div className="inline-flex flex-col space-y-[0.5rem] items-start justify-end px-5 pt-7 bg-white border rounded-lg border-grey-300 font-Urbanist" style={{ width: 370, height: 605, }} key={hackathon_id}>
                     <Link to={`/hackathons/${hackathon_id}`} className=" contents">
                         <img className="w-full h-1/2 bg-gray-300 rounded-lg" src={image} />
-                        <p className="w-auto h-8 text-2xl font-semibold leading-loose text-left text-brand-text-black">{title}</p>
-                        <p className="w-auto h-5 text-sm font-semibold leading-tight text-brand my-[0.59rem]">{date}</p>
-                        <p className="w-full h-1/5 text-sm font-semibold leading-tight text-brand-text-gray">{description}</p>
-                        <p className="w-auto h-5 text-sm font-semibold leading-tight text-gray-500">Sponsored by</p>
-                        <div className="flex flex-row">
+                        <p className="w-auto h-[3rem] text-xl leading-6 text-left text-brand-text-black break-normal  mt-[0.5rem]">{title}</p>
+                        <p className="w-auto text-sm font-semibold leading-tight text-brand mt-[0.5rem]">{date}</p>
+                        <p className="w-full h-[8rem] overflow-hidden text-sm leading-tight text-brand-text-gray  break-normal mt-[0.5rem]">{description}</p>
+                        <p className="w-auto h-5 text-sm leading-tight text-gray-500 mt-[0.5rem]">Sponsored by</p>
+                        <div className="flex flex-row mt-[0.5rem] h-[36px] w-full overflow-hidden">
                             {sponsored.split(',').map((url, index) => (
-                                <div key={index}>
-                                    <img className="w-9 h-9 mr-2" src={url} />
-                                </div>
+                                <img className="mr-[0.5rem]" src={url} key={index} />
                             ))}
                         </div>
                     </Link>
-                    <div className="inline-flex flex-row items-center justify-between py-[1.5rem]">
-                        <a className="text-sm leading-snug text-brand w-[10.88rem] h-7 py-1 bg-white border rounded border-brand mr-[0.81rem] text-center" href={discord_url} rel="external" title="Go to the Discroid channel" target="_blank">Join discord</a>
-                        <button className="text-sm leading-snug bg-brand w-[10.88rem] h-7 py-1 border rounded text-white" onClick={props.onOpenJoinNowDialogChange}>Join now</button>
+                    <div className="inline-flex flex-row items-center justify-between py-[1rem]">
+                        <a className="text-sm leading-snug text-brand w-[9.88rem] h-[1.75rem] py-1 bg-white border rounded border-brand mr-[0.81rem] text-center" href={discord_url} rel="external" title="Go to the Discroid channel" target="_blank">Join discord</a>
+                        <button className="text-sm leading-snug bg-brand  w-[9.88rem] h-[1.75rem] py-1 border rounded text-white" onClick={props.onOpenJoinNowDialogChange}>Join now</button>
                     </div>
                 </div>
             ))}
