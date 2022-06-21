3be3184fbc41e15548652f719dc7f3abdf6b1570
coton <coton.chen@gmail.com>
2022-06-21 15:43:08 +0800

    🐞 fix(/pages/Hackahtons/detail): 新增joinNow功能

--- src/nifities_assets/src/pages/Hackathons/detail.tsx
+++ src/nifities_assets/src/pages/Hackathons/detail.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import { Hackathon, fetchById, IApiData } from '../../api/hackahton';
 import PageNotFound from 'components/404';
 import PageError from 'components/500';
+import JoinNowDialog from './components/dialog';
 
 const queryClient = new QueryClient();
 
@@ -18,6 +19,13 @@ export default function HackathonsDetail() {
 
 function Detail() {
     let { id } = useParams<{ id: string }>();
+
+    // Join Now Dialog
+    const [openJoinNowDialog, setOpenJoinNowDialog] = React.useState(false);
+
+    const handleOpenJoinNowDialogChange = () => {
+        setOpenJoinNowDialog(openJoinNowDialog ? false : true);
+    };
 
     // 获取黑客松活动列表数据
     const { status, error, data } = useQuery<IApiData, Error>(`hackathons/detail/${id}`, () => fetchById(id));
@@ -38,50 +46,59 @@ function Detail() {
     const hackathon = data.data as Hackathon;
 
     return (
-        <div className='mx-auto w-[1190px] my-[2.06rem]'>
-            <section>
-                <div className='mx-[7.06rem] h-[21.63rem] flex flex-col justify-center relative'>
-                    <img className="w-full h-full rounded-sm" src={bannerImage} />
-                    <div className="w-full h-full absolute text-center">
-                        <img className="w-[12.19rem] h-auto bg-gray-100 mx-auto mt-[9rem] " src={hackathon.image} />
+        <>
+            <div className='mx-auto w-[1190px] my-[2.06rem]'>
+                <section>
+                    <div className='mx-[7.06rem] h-[21.63rem] flex flex-col justify-center relative'>
+                        <img className="w-full h-full rounded-sm" src={bannerImage} />
+                        <div className="w-full h-full absolute text-center">
+                            <img className="w-[12.19rem] h-auto bg-gray-100 mx-auto mt-[9rem] " src={hackathon.image} />
+                        </div>
                     </div>
-                </div>
-            </section>
+                </section>
 
-            <section>
-                <div className="mx-[7.06rem] relative -top-[1.38rem]">
+                <section>
+                    <div className="mx-[7.06rem] relative -top-[1.38rem]">
 
-                    <div className="mx-auto w-auto h-[11.69rem] bg-white border rounded-lg border-gray-300 text-left flex flex-row justify-between align-middle" >
-                        <div className="inline-flex flex-col space-y-5 items-start justify-start mx-[3.38rem] my-auto" style={{ width: 701, height: 140, }}>
-                            <div className=" h-[5rem] overflow-hidden ">
-                                <p className="text-4xl font-medium leading-10 text-brand-black text-Poppins break-normal">{hackathon.title}</p>
+                        <div className="mx-auto w-auto h-[11.69rem] bg-white border rounded-lg border-gray-300 text-left flex flex-row justify-between align-middle" >
+                            <div className="inline-flex flex-col space-y-5 items-start justify-start mx-[3.38rem] my-auto" style={{ width: 701, height: 140, }}>
+                                <div className=" h-[5rem] overflow-hidden ">
+                                    <p className="text-4xl font-medium leading-10 text-brand-black text-Poppins break-normal">{hackathon.title}</p>
+                                </div>
+                                <p className="text-xl font-medium leading-loose text-brand text-Urbanist">{hackathon.date}</p>
                             </div>
-                            <p className="text-xl font-medium leading-loose text-brand text-Urbanist">{hackathon.date}</p>
-                        </div>
-                        <div className="inline-flex flex-col items-end justify-center my-auto space-y-[0.88rem] w-2/5 mx-[3.38rem]">
-                            <a className={`text-sm leading-snug text-brand w-[10.88rem] h-7 py-1 bg-white border rounded border-brand text-center ${ hackathon.discord_url != null ? '' : 'hidden' }`} href={hackathon.discord_url} rel="external" title="Go to the Discroid channel" target="_blank">Join discord</a>
-                            <button className="text-sm leading-snug bg-brand w-[10.88rem] h-7 py-1 border rounded text-white">Join now</button>
-                        </div>
-                    </div>
+                            <div className="inline-flex flex-col items-end justify-center my-auto space-y-[0.88rem] w-2/5 mx-[3.38rem]">
+                                <a className={`text-sm leading-snug text-brand w-[10.88rem] h-7 py-1 bg-white border rounded border-brand text-center ${hackathon.discord_url != null ? '' : 'hidden'}`} href={hackathon.discord_url} rel="external" title="Go to the Discroid channel" target="_blank">Join discord</a>
 
-
-                    <div className="mx-auto mt-[1.56rem] px-[3.38rem] py-[1.6rem] bg-white border rounded-lg border-gray-300">
-                        <div className="text-left">
-                            <p className="text-2xl font-medium leading-loose text-gray-500 font-Urbanist">Sponsored by</p>
-                            <div className="flex flex-row flex-wrap justify-start align-middle mt-[1rem] space-y-[1rem] text-center">
-                                {hackathon.sponsored.split(',').map((url, index) => (
-                                    <img className=" m-auto" src={url} key={index} />
-                                ))}
+                                {hackathon.join_flag === null ? (
+                                    < button className="text-sm leading-snug bg-brand w-[10.88rem] h-7 py-1 border rounded text-white" onClick={handleOpenJoinNowDialogChange}>Join now</button>
+                                ) : (
+                                    < button className="text-sm leading-snug bg-brand  w-[9.88rem] h-[1.75rem] py-1 border rounded text-white disabled" >Joined</button>
+                                )}
                             </div>
                         </div>
-                    </div>
 
-                    <div className="mx-auto mt-[1.56rem] px-[3.38rem] py-[1.6rem] bg-white border rounded-lg border-gray-300 text-center">
-                        <img className="px-[3.38] m-auto" src={hackathon.image} />
-                        <div className="text-left px-[3.38] mt-[2rem]" dangerouslySetInnerHTML={{ __html: hackathon.content ? hackathon.content : '' }} />
+
+                        <div className="mx-auto mt-[1.56rem] px-[3.38rem] py-[1.6rem] bg-white border rounded-lg border-gray-300">
+                            <div className="text-left">
+                                <p className="text-2xl font-medium leading-loose text-gray-500 font-Urbanist">Sponsored by</p>
+                                <div className="flex flex-row flex-wrap justify-start align-middle mt-[1rem] space-y-[1rem] text-center">
+                                    {hackathon.sponsored.split(',').map((url, index) => (
+                                        <img className=" m-auto" src={url} key={index} />
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="mx-auto mt-[1.56rem] px-[3.38rem] py-[1.6rem] bg-white border rounded-lg border-gray-300 text-center">
+                            <img className="px-[3.38] m-auto" src={hackathon.image} />
+                            <div className="text-left px-[3.38] mt-[2rem]" dangerouslySetInnerHTML={{ __html: hackathon.content ? hackathon.content : '' }} />
+                        </div>
                     </div>
-                </div>
-            </section>
-        </div>
+                </section>
+            </div >
+
+            <JoinNowDialog open={openJoinNowDialog} onOpenJoinNowDialogChange={handleOpenJoinNowDialogChange} id={hackathon.hackathon_id} />
+        </>
     );
 };
